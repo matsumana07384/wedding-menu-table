5e385a232a57b7bbc0877ff725565040824f73f8
matsumana07384 <manami.m.227@outlook.jp>
2021-04-25 16:09:25 +0900

    add:Node.jsとExpressでローカルサーバーを構築する(2) ―Expressでルーティング―

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
-const http = require('http');
-const server = http.createServer((req, res)=>{
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!!');
+const express = require('express');
+const app = express();
+// パス指定用モジュール
+const path = require('path');
+
+// 8080番ポートで待ちうける
+app.listen(8080, () => {
+  console.log('Running at Port 8080...');
 });
-server.listen(8080);
+
+// 静的ファイルのルーティング
+app.use(express.static(path.join(__dirname, 'public')));
+
+// その他のリクエストに対する404エラー
+app.use((req, res) => {
+  res.sendStatus(404);
+});
