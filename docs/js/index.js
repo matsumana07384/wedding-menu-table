27042c121d621be8dfb94c4fc10e707506e4d307
matsumana07384 <manami.m.227@outlook.jp>
2021-04-29 15:16:49 +0900

    updade:関数化

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,9 @@
 // HTMLロード完了時
 window.onload = function () {
-    // お品書き開閉ボタンをタップすることで、画面ロードと同時にお品書きを開く
-    document.getElementById("book-button").click();
+  openMenu();
+}
+
+function openMenu() {
+  // お品書き開閉ボタンをタップすることで、画面ロードと同時にお品書きを開く
+  document.getElementById("book-button").click();
 }
